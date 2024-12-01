af6c28fd639ed4a71038d73bda2aa100ab7fa7f9
Lucas Samson <lsamson0012@gmail.com>
2024-12-01 15:56:52 -0600

    first commit

---
+++ socketServer.js
@@ -0,0 +1,20 @@
+import { Server } from 'socket.io';
+
+const io = new Server({
+    cors: {
+        origin: "*", // Allow all origins (adjust for security)
+        methods: ["GET", "POST"],
+    },
+});
+io.on("connection", function (socket) {
+    console.log("New client connected: ".concat(socket.id));
+    socket.on("orderPaid", function (data) {
+        console.log("Order Paid Received on Server:", data); // Log the received data
+        io.emit("orderPaid", data); // Broadcast to all clients
+    });
+    socket.on("disconnect", function () {
+        console.log("Client disconnected: ".concat(socket.id));
+    });
+});
+io.listen(4000); // Start WebSocket server on port 4000
+console.log("WebSocket server running on port 4000");
